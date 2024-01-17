57c9ca79966723806cecfc0e177fb34c0630a988
HjalmarVaikuell <vaikuell.hjalmar@gmail.com>
2024-01-17 10:48:45 +0200

    Updated dependancies
    Updated dependancies and downloaded chart.js

---
+++ under-pressure/src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="range" min="1" max="100" value="50">
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class HelloWorld extends Vue {
+  msg!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+input {
+  background-color: aquamarine;
+  align-self: center;
+}
+</style>
