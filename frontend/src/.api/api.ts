fd7c9cea84d41cc769cffe56e53fae8f8f3b4c65
var-pi <stefanehin4@gmail.com>
2024-01-23 12:53:15 +0200

    new api folder added

---
+++ frontend/src/.api/api.ts
@@ -0,0 +1,18 @@
+import { getSubjects } from "./endpoints/getSubjects";
+import { getMySubjects } from "./endpoints/getMySubjects";
+import { getEntries } from "./endpoints/getEntries";
+import { followSubject } from "./endpoints/followSubject";
+import { unfollowSubject } from "./endpoints/unfollowSubject";
+import { setEntry } from "./endpoints/setEntry";
+import { createUser } from "./endpoints/createUser";
+
+export {
+  getSubjects,
+  getMySubjects,
+  getEntries,
+  followSubject,
+  unfollowSubject,
+  setEntry,
+  createUser,
+};
+
