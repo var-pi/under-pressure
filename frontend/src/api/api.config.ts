3c39cbdf69a81fdc675ae7991d28f67d27fbd2ae
var-pi <stefanehin4@gmail.com>
2024-02-08 00:06:39 +0200

    handle UNAUTHORIZED and log in

--- frontend/src/api/api.config.ts
+++ frontend/src/api/api.config.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "https://efd0-2001-bb8-2002-98-bdb3-dd6f-41d1-b4f3.ngrok-free.app";
+// const BASE_URL = "https://efd0-2001-bb8-2002-98-bdb3-dd6f-41d1-b4f3.ngrok-free.app";
+const BASE_URL = "http://localhost:9091";
 
 export { BASE_URL };
 
