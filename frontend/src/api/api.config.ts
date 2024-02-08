72dba4ecd6832c2d02deedfff99522dad9838910
var-pi <stefanehin4@gmail.com>
2024-02-08 13:39:22 +0200

    changed BASE_URL

--- frontend/src/api/api.config.ts
+++ frontend/src/api/api.config.ts
@@ -1,5 +1,4 @@
-// const BASE_URL = "https://efd0-2001-bb8-2002-98-bdb3-dd6f-41d1-b4f3.ngrok-free.app";
-const BASE_URL = "http://localhost:9091";
+const BASE_URL = "https://under-pressure-backend.onrender.com";
 
 export { BASE_URL };
 
