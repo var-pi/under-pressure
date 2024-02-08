631370477e04d35cdf2a53bb1953e38b02c581bd
Hjalmar Vaiküll <96018839+HjalmarVaikuell@users.noreply.github.com>
2024-02-08 16:54:10 +0200

    Modal menu (#26)
    * Basic ModalMenu component
    
    +Working open and close ModalMenu
    
    * Hardcoded ModalMenu
    
    +Added ModalMenu with hardcoded style
    +Removed DropdownSubjects from MainPage
    
    * Modal menu basic functionality
    
    +Added basic functionality to modal menu
    +Settings button is now Menu button with styling
    +Removed DropdownSubjects

---
+++ frontend/src/components/ModalMenu.vue
@@ -0,0 +1,161 @@
+<template>
+  <div v-if="props.isOpen" class="modal-mask">
+    <div class="modal-wrapper">
+      <div ref="target" class="modal-container">
+        <div class="modal-header">
+          <button id="close-btn" @click.stop="emit('modal-close')">
+              X
+          </button>
+        </div>
+        <div class="modal-body">
+          <LoaderComponent 
+            :loading="isLoading" />
+          <div
+            v-if="!isLoading"
+            class="dropdown-content">
+            <input
+              id="search-bar"
+              v-model="filter"
+              type="text"
+              placeholder="🔍" />
+            <div class="scrollable-content">
+              <button
+                v-for="subjectItem in filteredSubjects"
+                :key="subjectItem.text"
+                :style="{ display: subjectItem.display }"
+                class="menu-btn"
+                @click="addFollowedSubject(subjectItem.text)">
+                {{ subjectItem.text }}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, defineProps, defineEmits, watch } from "vue";
+import { onClickOutside } from '@vueuse/core';
+import { api } from "@/api/api";
+
+import LoaderComponent from "@/components/LoaderComponent.vue";
+import { Subject } from "@/api/types";
+
+const props = defineProps({
+  isOpen: Boolean,
+});
+
+const emit = defineEmits(["modal-close"]);
+
+const target = ref(null)
+onClickOutside(target, ()=>emit('modal-close'))
+
+const filter = ref<string>("");
+const isDropdownVisible = ref(false);
+const allSubjects = ref([] as string[]);
+let isLoading = ref<boolean>(false);
+// Filtering subjects according to search-bar input
+const filteredSubjects = computed(function() {
+  const filterText = filter.value.toUpperCase();
+  return allSubjects.value.map(function(subject) {
+    return {
+      text: subject,
+      display: subject.toUpperCase().includes(filterText) ? "block" : "none",
+    };
+  });
+});
+
+async function toggleAllSubjects() {
+  if (!isDropdownVisible.value) {
+    isLoading.value = true;
+    filter.value = "";
+    await getAllSubjects();
+  }
+  isLoading.value = false;
+  isDropdownVisible.value = !isDropdownVisible.value;
+}
+
+async function getAllSubjects() {
+  try {
+    const subjects: Subject[] = await api.getSubjectsAll();
+    // Check if the ApiResponse is not null before extracting the value
+    if (subjects) {
+      // Extract the array of strings
+      allSubjects.value = subjects;
+    } else {
+      console.error("Failed to get subjects.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function addFollowedSubject(subjectName: string) {
+  await api.followSubject(subjectName);
+}
+
+watch(() => props.isOpen, toggleAllSubjects);
+
+</script>
+
+<style scoped>
+@import "@/styles/colors/colors.css";
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal-container {
+  width: 400px;
+  margin: 20vh auto;
+  padding: 20px 30px;
+  background-color: var(--col-3);
+  border-radius: 5px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+}
+.modal-header {
+  display: flex;
+  justify-content: center;
+}
+.modal-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10px 30px 10px 30px;
+}
+.scrollable-content {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+.scrollable-content::-webkit-scrollbar {
+    display: none;
+}
+.dropdown-content {
+  justify-content: center;
+  width: 340px;
+}
+#search-bar {
+  width: 332px;
+}
+.menu-btn {
+  width: 340px;
+}
+#close-btn {
+  margin-left: auto;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+</style>
--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,70 +1,93 @@
 <template>
-  <button id="settings-btn">Settings</button>
+  <button id="menu-btn" @click="openModal">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+  <ModalMenu id="menuModal" :is-open="isModalOpened" @modal-close="closeModal">
+  </ModalMenu>
   <div id="contents">
-    <LineGraph :new-selected-subject="newSelectedSubject" />
-    <div id="dropdown-menus">
-      <DropdownSubjects id="all-subjects" />
-      <DropdownPersonalSubjects
-        id="personal-subjects"
-        @handle-selected-subject-update="updateSelectedSubject"
-      />
-    </div>
+    <LineGraph
+      :new-selected-subject="newSelectedSubject" />
+    <DropdownPersonalSubjects
+      id="personal-subjects"
+      @handle-selected-subject-update="updateSelectedSubject" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import DropdownSubjects from "@/components/DropdownSubjects.vue";
 import DropdownPersonalSubjects from "@/components/DropdownPersonalSubjects.vue";
 import LineGraph from "@/components/LineGraph.vue";
+import ModalMenu from "@/components/ModalMenu.vue";
 
-const newSelectedSubject = ref("");
+const newSelectedSubject = ref("")
+let isModalOpened = ref(false);
 
 function updateSelectedSubject(subject: string) {
   newSelectedSubject.value = subject;
 }
+
+const openModal = () => {  
+  isModalOpened.value = true;
+};
+const closeModal = () => {
+  isModalOpened.value = false;
+};
 </script>
 
 <style scoped>
 @import "@/styles/colors/colors.css";
 
-#dropdown-menus {
-  display: flex;
-  justify-content: center;
-}
-#all-subjects {
-  margin: 1cap;
-}
 #personal-subjects {
   margin: 1cap;
 }
-#settings-btn {
-  max-width: 70px;
-  max-height: 70px;
-  width: 10vw;
-  height: 10vw;
-  aspect-ratio: 1/1;
-  margin: 10px;
-  position: fixed;
+#menu-btn {
+  position: absolute;
+  width: 35px;
+  height: 25px;
   top: 10px;
   right: 10px;
+  border: none;
+  background-color: transparent;
 }
-
+#menu-btn > span {
+  display: block;
+  background-color: var(--col-3);
+  margin: 5px 0;
+  height: 5px;
+  width: 35px;
+  max-width: 49px;
+}
+#menu-btn > span:nth-child(1) {
+  margin-top: 0px;
+}
+#menu-btn > span:nth-child(3) {
+  margin-bottom: 0px;
+}
 @media screen and (max-width: 550px) {
-  #settings-btn {
+  #menu-btn {
     top: 0;
     right: 0;
     width: 30px;
     height: 30px;
     margin: 5px;
   }
+  #menu-btn > span {
+    margin: 6px 0;
+    height: 6px;
+    width: 30px;
+  }
 }
-
 @media screen and (max-width: 400px) {
-  #settings-btn {
+  #menu-btn {
     width: 20px;
     height: 20px;
-    margin: 5px;
+  }
+  #menu-btn > span {
+    margin: 4px 0;
+    height: 4px;
+    width: 20px;
   }
 }
 </style>
