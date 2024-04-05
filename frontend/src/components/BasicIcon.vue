60cc89a6a8db4a9fc3942fa34a4f4badeff356f4
var-pi <stefanehin4@gmail.com>
2024-04-05 11:49:07 +0300

    Fixed BasicIcon

--- frontend/src/components/BasicIcon.vue
+++ frontend/src/components/BasicIcon.vue
@@ -1,15 +1,17 @@
 <template>
-  <img id="icon" :src="src" :alt="alt" />
+  <img id="icon" :src :alt="alt" />
 </template>
 
 <script setup lang="ts">
-import { withDefaults } from "vue";
+import { computed, withDefaults } from "vue";
 // import src from `../../assets/icons/${props.name}`;
-import src from "../../assets/icons/check.png"; // TODO!!
+// import src from "../../assets/icons/check.png"; // TODO!!
 
 const props = withDefaults(defineProps<{ name: string; alt?: string }>(), {
   alt: "",
 });
+
+const src = new URL(`../../assets/icons/${props.name}`, import.meta.url).href;
 </script>
 
 <style scoped>
