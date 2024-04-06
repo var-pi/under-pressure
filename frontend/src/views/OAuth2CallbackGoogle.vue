e394ff22c0b02c1f65b09260d95e4ff93f23b4c6
var-pi <stefanehin4@gmail.com>
2024-04-06 10:32:18 +0300

    Fixed bug of authorization fails

--- frontend/src/views/OAuth2CallbackGoogle.vue
+++ frontend/src/views/OAuth2CallbackGoogle.vue
@@ -1,20 +1,32 @@
+<template>
+  <LoaderComponent id="loading" loading />
+</template>
+
 <script setup lang="ts">
 import { onMounted } from "vue";
 import router from "@/router";
 import server from "@/api/server";
+import LoaderComponent from "@/components/LoaderComponent.vue";
 
 onMounted(() => {
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
 
-  if (code) {
-    // Now you have the authorization code, and you can send it to your backend
-    server
-      .post<{ code: string }, string>("/auth", { code })
-      .then((r) => sessionStorage.setItem("idTokenString", r))
-      .catch((e) => console.error(e));
-  }
+  if (!code) return router.push({ name: "home" });
 
-  router.push({ name: "home" });
+  // Now you have the authorization code, and you can send it to your backend
+  server
+    .post<{ code: string }, string>("/auth", { code })
+    .then((r) => sessionStorage.setItem("idTokenString", r))
+    .then(() => router.push({ name: "home" }));
 });
 </script>
+
+<style scoped>
+#loading {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  translate: -50% -50%;
+}
+</style>
