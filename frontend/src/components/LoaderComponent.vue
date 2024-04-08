4f12a8b066ef3c1978b3003fa9f7b9108d483c68
var-pi <stefanehin4@gmail.com>
2024-04-08 10:22:04 +0300

    refactored LoaderComponent

--- frontend/src/components/LoaderComponent.vue
+++ frontend/src/components/LoaderComponent.vue
@@ -1,48 +1,33 @@
 <!-- Loader by greyby: https://github.com/greyby/vue-spinner.git -->
 <template>
-  <div id="v-spinner" class="v-spinner">
-    <div class="v-sync v-sync1" :style="[spinnerDelay1]"></div>
-    <div class="v-sync v-sync2" :style="[spinnerDelay2]"></div>
-    <div class="v-sync v-sync3" :style="[spinnerDelay3]"></div>
+  <div id="v-spinner">
+    <div
+      v-for="n in [1, 2, 3]"
+      :key="n"
+      :style="{ animationDelay: `${70 * n}ms` }"
+    />
   </div>
 </template>
-
-<script setup lang="ts">
-import { computed } from "vue";
-
-const spinnerDelay1 = computed(() => ({
-  animationDelay: "0.07s",
-}));
-
-const spinnerDelay2 = computed(() => ({
-  animationDelay: "0.14s",
-}));
-
-const spinnerDelay3 = computed(() => ({
-  animationDelay: "0.21s",
-}));
-</script>
 
 <style>
 #v-spinner {
   display: flex;
-  height: 64px;
+  height: var(--default-size);
   justify-content: center;
   align-items: center;
-}
-
-.v-sync {
-  background-color: var(--col-fg-default);
-  height: 6px;
-  width: 6px;
-  margin: 2px;
-  border-radius: 100%;
-  display: inline-block;
-  animation-name: v-syncStretchDelay;
-  animation-duration: 0.6s;
-  animation-iteration-count: infinite;
-  animation-timing-function: ease-in-out;
-  animation-fill-mode: both;
+  & > div {
+    background-color: var(--col-fg-default);
+    height: 6px;
+    width: 6px;
+    margin: 2px;
+    border-radius: 100%;
+    display: inline-block;
+    animation-name: v-syncStretchDelay;
+    animation-duration: 0.6s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: both;
+  }
 }
 
 @-webkit-keyframes v-syncStretchDelay {
