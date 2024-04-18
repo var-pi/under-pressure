eadc33674ca06b66779fd2ea0fe256a3966af899
var-pi <stefanehin4@gmail.com>
2024-04-18 12:39:21 +0300

    Rudimentary

--- frontend/src/stores/event.ts
+++ frontend/src/stores/event.ts
@@ -6,8 +6,6 @@ import { defineStore } from "pinia";
 import { EntryUpdated } from "@/stores/events/entryUpdated";
 import { CurrentMissing } from "@/stores/events/currentMissing";
 import { PersonalMissing } from "@/stores/events/personalMissing";
-import { Followed } from "@/stores/events/followed";
-import { Unfollowed } from "@/stores/events/unflollowed";
 
 type Events = {
   // Entry of the currently selected subject has been updated.
@@ -16,10 +14,6 @@ type Events = {
   currentmissing: CurrentMissing;
   // Personal subjects have been request but none have been added yet.
   personalmissing: PersonalMissing;
-  // The subject has been followed
-  followed: Followed;
-  // The sbject has been unfollowed
-  unfollowed: Unfollowed;
 };
 
 export const useEventStore = defineStore("event", () => mitt<Events>());
--- frontend/src/stores/events/unflollowed.ts
+++
@@ -1,3 +0,0 @@
-export type Unfollowed = {
-  subject: string;
-};
