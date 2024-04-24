503c0e47358e51bb04b9eeb75141a5a1d26bb63d
HjalmarVaikuell <vaikuell.hjalmar@gmail.com>
2024-04-24 17:11:04 +0300

    Replaced isMobile with stored isMobile
    + Now using deviceStore.isMobile

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,9 +1,9 @@
 <template>
   <ModalMenu v-model:is-opened="isOpened.modal" />
 
-  <div id="chart-row" :class="{ mobile: isMobile }">
-    <LineGraph :is-mobile />
-    <SliderInput v-model="sliderValue" :is-vertical="!isMobile" />
+  <div id="chart-row" :class="{ mobile: deviceStore.isMobile }">
+    <LineGraph />
+    <SliderInput v-model="sliderValue" :is-vertical="!deviceStore.isMobile"  />
   </div>
   <div id="buttons-row">
     <ModalButton
@@ -16,27 +16,18 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from "vue";
+import { ref, watchEffect } from "vue";
 import DropdownPersonalSubjects from "@/components/DropdownPersonalSubjects.vue";
 import LineGraph from "@/components/LineGraph.vue";
 import ModalMenu from "@/components/ModalMenu.vue";
 import EntryButton from "@/components/buttons/EntryButton.vue";
 import ModalButton from "@/components/buttons/ModalButton.vue";
 import SliderInput from "@/components/SliderInput.vue";
+import { useDeviceStore } from "@/stores/device";
 
 const isOpened = ref({ modal: false, personal: false });
 const sliderValue = ref<number>(50);
-const isMobile = ref(false);
-const mobileMaxWidth = 900;
-
-onMounted(() => {
-  setIfVertical();
-  window.addEventListener("resize", setIfVertical);
-});
-
-function setIfVertical() {
-  isMobile.value = window.innerWidth < mobileMaxWidth;
-}
+const deviceStore = useDeviceStore();
 
 watchEffect(() => {
   if (isOpened.value.modal) isOpened.value.personal = false;
