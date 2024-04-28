c72a4d7810637c5f3f3c332656326dbaa80f3a3d
Stefan Ehin <66830771+var-pi@users.noreply.github.com>
2024-04-28 10:43:32 +0300

    81 smooth transitions (#136)
    * Moved background color to index.html
    
    * Moved "vh" logic to device store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,8 @@
 <template>
   <div id="container">
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
-
-<script setup lang="ts">
-import { onMounted } from "vue";
-
-onMounted(() => {
-  setVhProperty();
-});
-window.addEventListener("resize", () => {
-  setVhProperty();
-});
-
-function setVhProperty() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
-</script>
 
 <style scoped lang="scss">
 @import "@/styles/colors.css";
@@ -26,7 +10,6 @@ function setVhProperty() {
 #container {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: var(--col-bg-default);
   min-height: calc(var(--vh) * 100);
   display: flex;
   flex-direction: column;
--- frontend/src/stores/device.ts
+++ frontend/src/stores/device.ts
@@ -1,16 +1,30 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 
 export const useDeviceStore = defineStore("device", () => {
   const isMobile = ref(false);
 
-  const detectDeviceType = () => {
+  onMounted(() => {
+    detectDeviceType();
+    setVhProperty();
+
+    window.addEventListener("resize", detectDeviceType);
+    window.addEventListener("resize", setVhProperty);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("resize", detectDeviceType);
+    window.removeEventListener("resize", setVhProperty);
+  });
+
+  function detectDeviceType() {
     isMobile.value = window.innerWidth < 900;
-  };
+  }
 
-  detectDeviceType();
-
-  window.addEventListener("resize", detectDeviceType);
+  function setVhProperty() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
 
   return { isMobile };
 });
